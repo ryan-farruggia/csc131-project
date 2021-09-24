ace99a9205592207bb3fccbfa8d2bdb84ae3852e
Biscuit <rjfarruggia@csus.edu>
2021-09-24 01:39:11 -0700

    fix classpath to be relative

--- DataPiratesProject/src/main/java/main/PDFtest.java
+++ DataPiratesProject/src/main/java/main/PDFtest.java
@@ -9,13 +9,13 @@ import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 public class PDFtest {
     public static void main(String[] args) {
         try {
-            PDDocument pDDocument = PDDocument.load(new File("D:\\School\\Computer Science\\CSC131 Software Engineering\\Project\\pdf-java.pdf"));
+            PDDocument pDDocument = PDDocument.load(new File("pdf-java.pdf"));
             PDAcroForm pDAcroForm = pDDocument.getDocumentCatalog().getAcroForm();
             PDField field = pDAcroForm.getField("txt_1");
             field.setValue("This is a first field printed by Java");
             field = pDAcroForm.getField("txt_2");
             field.setValue("This is a second field printed by Java");
-            pDDocument.save("D:\\School\\Computer Science\\CSC131 Software Engineering\\Project\\pdf-java.pdf");
+            pDDocument.save("pdf-java.pdf");
             pDDocument.close();
         } catch (IOException e) {
             e.printStackTrace();
