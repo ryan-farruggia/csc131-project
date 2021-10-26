5f1f386b596dce0b0adda4b36d9ab14f92c99850
Biscuit <rjfarruggia@csus.edu>
2021-10-25 19:12:59 -0700

    add classes to be coded in future

---
+++ DataPiratesProject/src/main/java/main/contractNumber.java
@@ -0,0 +1,6 @@
+package main;
+
+public class contractNumber {
+	// this class makes the contract number at the top of each page the same
+	// as the one entered on Page 1 by the user
+}
---
+++ DataPiratesProject/src/main/java/main/eventListener.java
@@ -0,0 +1,6 @@
+package main;
+
+public class eventListener {
+	// this class actively listens to the PDF for any type of change, whether it's text being
+	// entered, a dropdown menu opened, or a button clicked
+}
